acd08afc062042f8d4e3fcc34a37fd024b310a5e
mrp_geeksltd <mrp@geeks.ltd.uk>
2021-09-10 15:07:37 +0100

    Add integration testing

---
+++ Tests/Ca.IntegrationTests/BlogPostTests/BlogPostTest.cs
@@ -0,0 +1,63 @@
+﻿using Ca.Core.Domain.Blog;
+using Ca.Services.DTOs.Blog;
+using Ca.WebApi;
+using FluentAssertions;
+using GenFu;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Ca.IntegrationTests
+{
+    public class BlogPostTest : IClassFixture<CustomWebApplicationFactory<Ca.WebApi.Startup>>
+    {
+        private readonly HttpClient _client;
+        private readonly CustomWebApplicationFactory<Startup> _factory;
+
+        public BlogPostTest(CustomWebApplicationFactory<Ca.WebApi.Startup> factory)
+        {
+            _factory = factory;
+            _client = factory.CreateClient(new WebApplicationFactoryClientOptions
+            {
+                AllowAutoRedirect = false
+            });
+        }
+
+        [Fact]
+        public async Task Post_CreateBlogPost()
+        {
+            var blogPost = CreateNewBlogPost();
+
+            var blogPostJson = JsonConvert.SerializeObject(blogPost);
+
+            HttpContent content = new StringContent(blogPostJson, Encoding.UTF8, "application/json");
+
+            HttpRequestMessage request = new()
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri("http://localhost:5000/api/BlogPost/add-post"),
+                Content = content
+            };
+
+            var result = await _client.SendAsync(request);
+
+            result.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+        }
+
+        private BlogPostDto CreateNewBlogPost()
+        {
+            A.Configure<BlogPostDto>()
+                .Fill(x => x.Body).AsLoremIpsumSentences(5)
+                .Fill(x => x.Title).AsArticleTitle()
+                .Fill(x => x.CreatedOn).AsPastDate();
+
+            var blogPost = A.New<BlogPostDto>();
+
+            return blogPost;
+        }
+    }
+}
---
+++ Tests/Ca.IntegrationTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,29 @@
+﻿using Ca.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+
+namespace Ca.IntegrationTests
+{
+    public class CustomWebApplicationFactory<TStartup>
+        : WebApplicationFactory<TStartup> where TStartup : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                var descriptor = services.SingleOrDefault(
+                    d => d.ServiceType == typeof(DbContextOptions<AppDbContext>));
+
+                services.Remove(descriptor);
+
+                services.AddDbContext<AppDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase("InMemoryDbForTesting");
+                });
+            });
+        }
+    }
+}
